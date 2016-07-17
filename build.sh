bcb5aa5634d6d6a9135d94738195fa4cc4df0583
James Christopher Adduono <jc@adduono.com>
2016-07-17 17:06:08 -0400

    Update build scripts

--- build.sh
+++ build.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
-# simple bash script for executing build
+# Kali NetHunter kernel for LG Google Nexus 5
+# Build script by jcadduono
 
-# root directory of NetHunter hammerhead git repo (default is this script's location)
+################### BEFORE STARTING ################
+#
+# download a working toolchain and extract it somewhere and configure this
+# file to point to the toolchain's root directory.
+#
+# once you've set up the config section how you like it, you can simply run
+# ./build.sh [DEVICE]
+#
+###################### CONFIG ######################
+
+# root directory of hammerhead kernel git repo (default is this script's location)
 RDIR=$(pwd)
 
 [ "$VER" ] ||
@@ -9,38 +20,35 @@ RDIR=$(pwd)
 VER=$(cat "$RDIR/VERSION")
 
 # directory containing cross-compile arm toolchain
-TOOLCHAIN=$HOME/build/toolchain/arm-cortex_a15-linux-gnueabihf-linaro_4.9.4-2015.06
+TOOLCHAIN=$HOME/build/toolchain/gcc-linaro-4.9-2016.02-x86_64_arm-linux-gnueabihf
 
 # amount of cpu threads to use in kernel make process
 THREADS=5
 
 ############## SCARY NO-TOUCHY STUFF ###############
 
-export ARCH=arm
-export CROSS_COMPILE=$TOOLCHAIN/bin/arm-cortex_a15-linux-gnueabihf-
-
-cd "$RDIR"
-
-[ "$TARGET" ] || TARGET=nethunter
-[ "$1" ] && {
-	DEVICE=$1
-} || {
-	DEVICE=hammerhead
-}
-DEFCONFIG=${TARGET}_${DEVICE}_defconfig
-
-[ -f "$RDIR/arch/$ARCH/configs/${DEFCONFIG}" ] || {
-	echo "Config $DEFCONFIG not found in $ARCH configs!"
-	exit 1
-}
-
-export LOCALVERSION=$TARGET-$DEVICE-$VER
-
 ABORT()
 {
-	echo "Error: $*"
+	[ "$1" ] && echo "Error: $*"
 	exit 1
 }
+
+export ARCH=arm
+export CROSS_COMPILE=$TOOLCHAIN/bin/arm-linux-gnueabihf-
+
+[ -x "${CROSS_COMPILE}gcc" ] ||
+ABORT "Unable to find gcc cross-compiler at location: ${CROSS_COMPILE}gcc"
+
+[ "$1" ] && DEVICE=$1
+[ "$DEVICE" ] || DEVICE=hammerhead
+[ "$TARGET" ] || TARGET=nethunter
+
+DEFCONFIG=${TARGET}_${DEVICE}_defconfig
+
+[ -f "$RDIR/arch/$ARCH/configs/${DEFCONFIG}" ] ||
+ABORT "Config $DEFCONFIG not found in $ARCH configs!"
+
+export LOCALVERSION=$TARGET-$DEVICE-$VER
 
 CLEAN_BUILD()
 {
@@ -54,7 +62,8 @@ SETUP_BUILD()
 	echo "Creating kernel config for $LOCALVERSION..."
 	cd "$RDIR"
 	mkdir -p build
-	make -C "$RDIR" O=build "$DEFCONFIG" || ABORT "Failed to set up build"
+	make -C "$RDIR" O=build "$DEFCONFIG" \
+		|| ABORT "Failed to set up build"
 }
 
 BUILD_KERNEL()
--- menuconfig.sh
+++ menuconfig.sh
@@ -1,30 +1,33 @@
 #!/bin/bash
 # simple script for executing menuconfig
 
-# root directory of NetHunter hammerhead git repo (default is this script's location)
+# root directory of hammerhead kernel git repo (default is this script's location)
 RDIR=$(pwd)
 
 # directory containing cross-compile arm toolchain
-TOOLCHAIN=$HOME/build/toolchain/arm-cortex_a15-linux-gnueabihf-linaro_4.9.4-2015.06
-
-############## SCARY NO-TOUCHY STUFF ###############
+TOOLCHAIN=$HOME/build/toolchain/gcc-linaro-4.9-2016.02-x86_64_arm-linux-gnueabihf
 
 export ARCH=arm
-export CROSS_COMPILE=$TOOLCHAIN/bin/arm-cortex_a15-linux-gnueabihf-
+export CROSS_COMPILE=$TOOLCHAIN/bin/arm-linux-gnueabihf-
 
-[ "$TARGET" ] || TARGET=nethunter
-[ "$1" ] && {
-	DEVICE=$1
-} || {
-	DEVICE=hammerhead
+ABORT()
+{
+	[ "$1" ] && echo "Error: $*"
+	exit 1
 }
+
+[ -x "${CROSS_COMPILE}gcc" ] ||
+ABORT "Unable to find gcc cross-compiler at location: ${CROSS_COMPILE}gcc"
+
+[ "$1" ] && DEVICE=$1
+[ "$DEVICE" ] || DEVICE=hammerhead
+[ "$TARGET" ] || TARGET=nethunter
+
 DEFCONFIG=${TARGET}_${DEVICE}_defconfig
 DEFCONFIG_FILE=$RDIR/arch/$ARCH/configs/$DEFCONFIG
 
-[ -f "$DEFCONFIG_FILE" ] || {
-	echo "Config $DEFCONFIG not found in $ARCH configs!"
-	exit 1
-}
+[ -f "$DEFCONFIG_FILE" ] ||
+ABORT "Config $DEFCONFIG not found in $ARCH configs!"
 
 cd "$RDIR"
 echo "Cleaning build..."
@@ -33,13 +36,13 @@ mkdir build
 make -s -i -C "$RDIR" O=build "$DEFCONFIG" menuconfig
 echo "Showing differences between old config and new config"
 echo "-----------------------------------------------------"
-command -v colordiff >/dev/null 2>&1 && {
+if command -v colordiff >/dev/null 2>&1; then
 	diff -Bwu --label "old config" "$DEFCONFIG_FILE" --label "new config" build/.config | colordiff
-} || {
+else
 	diff -Bwu --label "old config" "$DEFCONFIG_FILE" --label "new config" build/.config
 	echo "-----------------------------------------------------"
 	echo "Consider installing the colordiff package to make diffs easier to read"
-}
+fi
 echo "-----------------------------------------------------"
 echo -n "Are you satisfied with these changes? Y/N: "
 read option
